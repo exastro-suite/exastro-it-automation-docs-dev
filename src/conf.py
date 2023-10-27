5c5607551c8b36ccda78520794c52776b87efc65
Wreath Vine <wreath.vine@gmail.com>
2023-10-27 09:18:21 +0000

    多言語化準備

--- src/conf.py
+++ src/conf.py
@@ -24,6 +24,7 @@ author = 'NEC'
 # The full version, including alpha/beta/rc tags
 release = ''
 
+locale_dirs = ['locale/']
 gettext_compact = False
 
 # -- General configuration ---------------------------------------------------
@@ -50,6 +51,7 @@ templates_path = ['_templates']
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = 'ja'
+languages = ["ja", "en"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
